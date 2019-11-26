e0c170ad68287f7129554100c1675ac739555a86
Paul Blay <paulmblay@gmail.com>
2019-11-26 21:33:41 +0000

    Started item 3

--- src/example_code.cpp
+++ src/example_code.cpp
@@ -42,7 +42,7 @@ class ConstComp{
 #define IS_GREATER(a,b) ((a) > (b) ? (a) : (b))
 
 // the irony is that these test cases are clearly pre-procesor macros =D
-TEST_CASE("#define increment once", "[Item 2.2]") {
+TEST_CASE("#define increment once", "[Item 2.0]") {
     int a=5,b=0;
     IS_GREATER(++a,b+10);
     REQUIRE(a == 6); // Lovely, as you might expect, ++a == 6
@@ -64,21 +64,54 @@ inline T isGreater(const T& a, const T& b)
     return (a > b ? a : b);
 }
 
-TEST_CASE("inline increment once", "[Item 2.3]") {
+TEST_CASE("inline increment once", "[Item 2.2]") {
     int a=5,b=0;
     (void)isGreater(++a,b+10);
     REQUIRE(a == 6); // Lovely, as you might expect, ++a == 6
 }
 
-TEST_CASE("inline still increment once", "[Item 2.4]") {
+TEST_CASE("inline still increment once", "[Item 2.3]") {
     int a=5,b=0;
     (void)isGreater(++a,b);
     REQUIRE(a == 6); // Lovely, as you might expect, ++a == 6
 }
 
 /***************************************************************************************************/
-/*
+/* Item 3: Use const whenever possible
 /***************************************************************************************************/
+
+TEST_CASE("Const pointers and data", "[Item 3.0]") {
+    char greeting[] = "Hello";
+
+    char *p = greeting;             // non-const pointer & data
+
+    const char *q = greeting;       // non-const pointer & const data
+
+    char * const r = greeting;      // const pointer & non-const data
+    char const *s = greeting;       // SAME - const pointer & non-const data (I prefer this)
+
+    const char * const t = greeting;// const pointer & const data
+
+    REQUIRE(1==1);
+}
+
+class Rational{
+    int i;
+};
+
+Rational A,B,C;
+Rational operator*(const Rational& lhs, const Rational& rhs)
+{
+        return (A); // whatever, not important for the demonstration
+}
+
+TEST_CASE("Const function returns", "[Item 3.1]") {
+
+    A*B = C; /* A*B becomes equal to C? This is meaningless, but actually not prevented by the compiler
+                for non built-in types. */
+    // To prevent it, declare our * overload function to return a constant.
+    REQUIRE(1==1);
+}
 
 /***************************************************************************************************/
 /* Item 20: User defined types should be passed as 'reference to const'                            */
